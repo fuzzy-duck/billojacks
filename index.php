0e36f03098a827e4d12f7600f2590b3860fdda42
Rhys <rhys@fuzzyduck.co.uk>
2019-10-09 11:07:44 +0100

    additional blocks

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Owl -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css" />
+
     <!-- Style CSS -->
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/style.css' ?>">
 
@@ -68,6 +72,97 @@
         <div class="down"></div>
       </div>
     </section>
+
+    <section id="notebook">
+      <div class="nbook">
+        <div class="up"></div>
+          <h3>View case files</h3>
+      </div>
+      <div class="card">
+        <div class="card-bg"></div>
+        <div class="notes-title"></div> 
+        <h3>Open</h3>
+      </div>
+    </section>
+
+    <section id="clues">
+      <div class="card">
+        <div class="notebook-inner">
+            <h3 style="margin-bottom: -70px;">This is your notebook</h3>
+            <h3>Certain Information in the Notebook <span>has been underlined in red</span></h3>
+            <h3 style="margin-top: -50px;">Clicking on this information will add clues to your notebook</h3>
+        </div>
+      </div>
+    </section>
+
+    <section id="news-intro">
+      <div class="card">
+        <div class="card-bg">
+
+        <div class="upper">
+          <p>Tuesday 3rd April 1832</p><p>Saddleworth Museum</p>
+        </div>
+
+        <div class="lower">
+          <p class="lead">Frightful murders in Saddleworth</p>
+          <p>After almost 200 years, one of the most shocking crimes in history remains unsolved. But Saddleworth Museum investigators have opened the files again in a bid to uncover some answers for good. 
+            The case has long puzzled police who have yet to identify the person or persons responsible for the chilling murders of father and son, William, 84, and Thomas Bradbury, 46 of Greenfield, Saddleworth.  
+            Thomas was married, but lived with his elderly father at William’s pub – The Moorcock Inn, commonly known as ‘Bills o’ Jacks’.
+            The pub was nestled on the hillside along Holmfirth Road, making it a convenient stopover for travellers and ‘navvies’ working nearby. 
+            But as darkness fell upon the moors on 2nd April 1832, the normally unassuming inn was thrust into the spotlight as the scene of two horrific murders. 
+            Reuben Platt, a friend of the two men, was at the pub earlier that evening sharing a quiet drink with the father and son…</p>
+        </div>
+
+        </div>
+      </div>
+
+      <h3>Continue</h3>
+    </section>
+
+    <section id="report">
+      <div class="nav">
+        <nav>
+          <div class="hamburger"></div>
+          <div class="back"></div>
+          <div class="pg-num"></div>
+        </nav>
+      </div>
+      <div class="card">
+        <div class="card-bg">
+          <div class="content">
+            <div class="title">
+              <h1>Monday 2nd April 1832</h1>
+              <h3>6.30pm</h3>
+            </div>
+            <div class="body-para">
+            <p>The owner of The Moorcock Inn, William Bradbury (Bill), was joined by his son Thomas Bradbury (Tom) and friend Reuben Platt (63) for an evening drink at his pub.</p>
+            <p>Shortly after, Tom and Reuben set off on a 2 mile walk into Greenfield village – Tom to pick up some supplies at the village store, and Reuben to go home. Bill was left on his own at The Moorcock Inn.</p>
+            <p>Half a mile down the road, they encountered three men. In an Irish accent, one of them asked how far it was to Holmfirth. They told the men it was 8 miles or so.</p>
+            <p>According to Reuben, Tom felt uneasy: “I don’t like to leave my father as he is. I don’t like these men; they are a bad-looking set: they are Irishmen.” He also knew one of the men to be a thief who previously stole stockings from his father.</p>
+            <p>Tom and Reuben watched and waited until the men had walked on, up the path and beyond the pub. Only then did they continue walking to the village, parting company at Road End, Greenfield, to go their own separate ways.</p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="crime-scene">
+        <nav>
+          <div class="hamburger"></div>
+          <div class="back"></div>
+          <div class="pg-num"></div>
+        </nav>
+    </section>
+
+  
+
+
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
     
   </body>
 </html>
